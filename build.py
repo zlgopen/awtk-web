ecaf4b2101940e3b2ff11400cb4016a835da0d80
lixianjing <xianjimli@hotmail.com>
2019-05-03 08:53:37 +0800

    fix build scripts on windows

--- build.py
+++ build.py
@@ -4,15 +4,16 @@ import glob
 import json
 import shutil
 
-WEB_ROOT = os.path.abspath('webroot')
-AWTK_ROOT_DIR = os.path.abspath('../awtk')
-os.environ['AWTK_ROOT_DIR'] = AWTK_ROOT_DIR
-
 def join_path(root, subdir):
     return os.path.normpath(os.path.join(root, subdir))
 
+WEB_ROOT = os.path.abspath('webroot')
+AWTK_ROOT_DIR = os.path.abspath('../awtk')
+os.environ['AWTK_ROOT_DIR'] = AWTK_ROOT_DIR
 sys.path.append(join_path(AWTK_ROOT_DIR, 'staticcheck/common'))
+
 import awtk_files as awtk
+
 
 def mkdir_if_not_exist(fullpath):
     if os.path.exists(fullpath):
@@ -21,36 +22,61 @@ def mkdir_if_not_exist(fullpath):
         os.makedirs(fullpath)
 
 
-def copy_and_overwrite(from_path, to_path):
-    print(from_path + '=>' + to_path)
-    if os.path.exists(to_path):
-        shutil.rmtree(to_path)
-    shutil.copytree(from_path, to_path)
+def copy_folder(src, dst):
+    print(src + '=>' + dst)
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    shutil.copytree(src, dst)
 
 
-def get_app_target_dir(config):
+def copy_file(src, dst):
+    print(src + '=>' + dst)
+    with open(dst, 'w') as outfile:
+        with open(src) as infile:
+            outfile.write(infile.read())
+            outfile.write("\n")
+
+
+def merge_files(srcs, dst):
+    print(dst)
+    with open(dst, 'w') as outfile:
+        for fname in srcs:
+            print(fname)
+            with open(fname) as infile:
+                outfile.write(infile.read())
+                outfile.write("\n")
+
+
+def append_file(src, dst):
+    with open(dst, 'a') as outfile:
+        with open(src) as infile:
+            outfile.write(infile.read())
+            outfile.write("\n")
+
+
+def config_get_app_target_dir(config):
     return join_path(WEB_ROOT, config['name'])
 
 
-def get_js_dir(config):
-    return join_path(get_app_target_dir(config), "js")
+def config_get_js_dir(config):
+    return join_path(config_get_app_target_dir(config), "js")
 
 
-def get_target_assets_dir(config):
-    return join_path(get_app_target_dir(config), "assets")
+def config_get_target_assets_dir(config):
+    return join_path(config_get_app_target_dir(config), "assets")
 
 
-def get_src_assets_dir(src_app_root, config):
+def config_get_src_assets_dir(src_app_root, config):
     return join_path(src_app_root, config['assets'])
 
 
 def prepare_update_res(config):
     infile = 'data/update_res.py'
-    outfile = join_path(get_app_target_dir(config), 'update_res.py')
+    outfile = join_path(config_get_app_target_dir(config), 'update_res.py')
 
     fo = open(infile, "r+")
     str = fo.read()
-    str = str.replace('$AWTK_ROOT', AWTK_ROOT_DIR).replace('\\', '\\\\');
+    str = str.replace('$AWTK_ROOT', AWTK_ROOT_DIR).replace('\\', '\\\\')
     fo.close()
 
     fo = open(outfile, "w")
@@ -61,44 +87,22 @@ def prepare_update_res(config):
 
 
 def update_assets(config):
-    prepare_update_res(config);
-    get_app_target_dir(config)
+    prepare_update_res(config)
+    config_get_app_target_dir(config)
 
     cwd = os.getcwd()
-    app_target_dir = get_app_target_dir(config)
+    app_target_dir = config_get_app_target_dir(config)
     os.chdir(app_target_dir)
-    os.system(sys.executable + ' update_res.py all')
-    os.system(sys.executable + ' update_res.py json')
-    os.system(sys.executable + ' update_res.py web')
+    os.system('\"'+sys.executable+'\"' + ' update_res.py all')
+    os.system('\"'+sys.executable+'\"' + ' update_res.py json')
+    os.system('\"'+sys.executable+'\"' + ' update_res.py web')
     os.chdir(cwd)
 
 
-def merge_js_files(filenames, ouputjs):
-    print(ouputjs)
-    with open(ouputjs, 'w') as outfile:
-        for fname in filenames:
-            print(fname)
-            with open(fname) as infile:
-                outfile.write(infile.read())
-                outfile.write("\n");
-
-def append_js_file(filename, ouputjs):
-    with open(ouputjs, 'a') as outfile:
-        with open(filename) as infile:
-            outfile.write(infile.read())
-            outfile.write("\n");
-
-def copy_js_file(filename, ouputjs):
-    with open(ouputjs, 'a') as outfile:
-        with open(filename) as infile:
-            outfile.write(infile.read())
-            outfile.write("\n");
-
-
 def build_awtk_web_js(config):
-    app_target_dir = get_app_target_dir(config)
+    app_target_dir = config_get_app_target_dir(config)
     assert_js = join_path(app_target_dir, 'assets_web.js')
-    outfile = join_path(get_js_dir(config), 'awtk_web.js')
+    outfile = join_path(config_get_js_dir(config), 'awtk_web.js')
     awtk_web_js_files = [assert_js,
                          'src/js/browser.js',
                          'src/js/webgl2d.js',
@@ -113,32 +117,36 @@ def build_awtk_web_js(config):
                          'src/js/key_event.js',
                          'src/js/events_source.js',
                          'src/js/main_loop_web.js']
-    merge_js_files(awtk_web_js_files, outfile)
+    merge_files(awtk_web_js_files, outfile)
 
 
 def is_js_app(config):
     return config['app_type'] == 'js'
 
+
 def prepare_app_target_dir(config):
-    js_dir = get_js_dir(config)
+    js_dir = config_get_js_dir(config)
     mkdir_if_not_exist(js_dir)
 
+
 def copy_data_file(config, filename):
-    src_filename = 'data/' + filename;
-    target_filename = join_path(get_app_target_dir(config), filename);
-    shutil.copyfile(src_filename, target_filename);
+    src_filename = 'data/' + filename
+    target_filename = join_path(config_get_app_target_dir(config), filename)
+    shutil.copyfile(src_filename, target_filename)
+
 
 def build_app_assets(src_app_root, config):
-    target_assets_dir = get_target_assets_dir(config)
-    src_assets_dir = get_src_assets_dir(src_app_root, config)
-    copy_and_overwrite(src_assets_dir, target_assets_dir)
-    copy_data_file(config, 'app.html');
-    copy_data_file(config, 'index.html');
+    target_assets_dir = config_get_target_assets_dir(config)
+    src_assets_dir = config_get_src_assets_dir(src_app_root, config)
+    copy_folder(src_assets_dir, target_assets_dir)
+    copy_data_file(config, 'app.html')
+    copy_data_file(config, 'index.html')
     update_assets(config)
+
 
 def build_awtk_js(src_app_root, config, flags):
     cwd = os.path.abspath(os.getcwd())
-    app_target_dir = get_app_target_dir(config)
+    app_target_dir = config_get_app_target_dir(config)
     assert_c = join_path(app_target_dir, 'assets_web.c')
 
     app_files = [assert_c]
@@ -153,13 +161,14 @@ def build_awtk_js(src_app_root, config, flags):
 
     web_files = glob.glob('src/c/*.c')
     files = awtk.getWebFiles() + web_files + app_files
-    all_files = [];
+    all_files = []
     for f in files:
         all_files.append(os.path.normpath(os.path.abspath(f)))
-    
-    COMMON_FLAGS = ' ' + flags + ' -Werror ';
+
+    COMMON_FLAGS = ' ' + flags + ' -Werror '
     COMMON_FLAGS = COMMON_FLAGS + ' -DSAFE_HEAP=1 -DASSERTIONS=1 -DSTACK_OVERFLOW_CHECK=1 '
-    COMMON_FLAGS = COMMON_FLAGS + ' -s EXTRA_EXPORTED_RUNTIME_METHODS=@configs/export_runtime_funcs.json '
+    COMMON_FLAGS = COMMON_FLAGS + \
+        ' -s EXTRA_EXPORTED_RUNTIME_METHODS=@configs/export_runtime_funcs.json '
     if(is_js_app(config)):
         COMMON_FLAGS = COMMON_FLAGS + ' -s EXPORTED_FUNCTIONS=@configs/export_awtk_funcs.json '
         COMMON_FLAGS = COMMON_FLAGS + ' -DWITH_JS_APPLICATION_INIT=1 -DAWTK_WEB_JS '
@@ -169,40 +178,43 @@ def build_awtk_js(src_app_root, config, flags):
 
     COMMON_FLAGS = COMMON_FLAGS + ' -DHAS_STD_MALLOC -DNDEBUG -DAWTK_WEB -Isrc/c '
     COMMON_FLAGS = COMMON_FLAGS + ' -DWITH_WINDOW_ANIMATORS -DWITH_NANOVG_GPU '
-	
-    output = join_path(get_js_dir(config), "awtk.js")
-    CPPFLAGS_JS = ' -o ' + output + ' -s WASM=0 ' + COMMON_FLAGS;
+
+    output = join_path(config_get_js_dir(config), "awtk.js")
+    CPPFLAGS_JS = ' -o ' + output + ' -s WASM=0 ' + COMMON_FLAGS
     awtk.runArgsInFile('emcc -v ', CPPFLAGS_JS, all_files)
-    
-    output = join_path(get_js_dir(config), "awtk_asm.js")
-    CPPFLAGS_ASM = ' -o ' + output + COMMON_FLAGS;
+
+    output = join_path(config_get_js_dir(config), "awtk_asm.js")
+    CPPFLAGS_ASM = ' -o ' + output + COMMON_FLAGS
     awtk.runArgsInFile('emcc -v ', CPPFLAGS_ASM, all_files)
-   
-    app_files=[];
-    output = join_path(get_js_dir(config), "app.js")
+
+    app_files = []
+    output = join_path(config_get_js_dir(config), "app.js")
     if(is_js_app(config)):
-        app_files.append('gen/ts/awtk_api.js');
+        app_files.append('gen/ts/awtk_api.js')
     for f in sources:
         if f.endswith('.js'):
             app_files.append(join_path(src_app_root, f))
-    merge_js_files(app_files, output);
+    merge_files(app_files, output)
+
 
 action = 'all'
 filename = os.path.abspath('./demo.json')
 
+
 def show_usage():
     print('Usage: python build.py app.json action(all|debug|release|assets|awtk_web_js|awtk_js|js)')
-    print('=============================================================');
-    print('  debug:        build debug version.');
-    print('  release:      build release version.');
-    print('  assets:       build assets only.');
-    print('  awtk_js:      build awtk_js only.');
-    print('  awtk_web_js:  build awtk_web_js only.');
-    print('  js:           build awtk_js and awtk_web_js only.');
-    print('  all:          same as debug. build debug version.');
-    print('=============================================================');
+    print('=============================================================')
+    print('  debug:        build debug version.')
+    print('  release:      build release version.')
+    print('  assets:       build assets only.')
+    print('  awtk_js:      build awtk_js only.')
+    print('  awtk_web_js:  build awtk_web_js only.')
+    print('  js:           build awtk_js and awtk_web_js only.')
+    print('  all:          same as debug. build debug version.')
+    print('=============================================================')
 
     sys.exit(0)
+
 
 if len(sys.argv) < 3:
     show_usage()
@@ -211,17 +223,19 @@ else:
     action = sys.argv[2]
     filename = os.path.abspath(sys.argv[1])
 
+
 def clean_temp_files(config):
-    app_target_dir = get_app_target_dir(config)
-    os.remove(join_path(app_target_dir, 'update_res.py'));
-    os.remove(join_path(app_target_dir, 'assets.c'));
-    os.remove(join_path(app_target_dir, 'assets_web.c'));
-    os.remove(join_path(app_target_dir, 'assets_web.js'));
+    app_target_dir = config_get_app_target_dir(config)
+    os.remove(join_path(app_target_dir, 'update_res.py'))
+    os.remove(join_path(app_target_dir, 'assets.c'))
+    os.remove(join_path(app_target_dir, 'assets_web.c'))
+    os.remove(join_path(app_target_dir, 'assets_web.js'))
+
 
 with open(filename, 'r') as load_f:
     config = json.load(load_f)
     src_app_root = os.path.dirname(filename)
-    prepare_app_target_dir(config);
+    prepare_app_target_dir(config)
 
     if action == 'all':
         build_app_assets(src_app_root, config)
@@ -247,4 +261,3 @@ with open(filename, 'r') as load_f:
         build_awtk_js(src_app_root, config, '')
     else:
         show_usage()
-
--- src/c/main.c
+++ src/c/main.c
@@ -4,22 +4,23 @@
 #include "main_loop_web.h"
 
 extern ret_t assets_init(void);
-#ifdef WITH_JS_APPLICATION_INIT
+
+#ifdef AWTK_WEB_JS
 #include <emscripten.h>
 #else
 extern ret_t application_init(void);
-#endif/*WITH_JS_APPLICATION_INIT*/
+#endif/*AWTK_WEB_JS*/
 
 int32_t awtk_web_init(void) {
   tk_init(0, 0, APP_MOBILE, NULL, NULL);
   tk_ext_widgets_init();
 
   assets_init();
-#ifdef WITH_JS_APPLICATION_INIT
+#ifdef AWTK_WEB_JS
   EM_ASM_INT({ TBrowser.loadScript("js/app.js"); }, 0);
 #else
   application_init();
-#endif/*WITH_JS_APPLICATION_INIT*/
+#endif/*AWTK_WEB_JS*/
 
   return TRUE;
 }
