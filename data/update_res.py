a29a1a14d177be099062eb92994ac6e7f0f7e572
xianjimli <xianjimli@gmail.com>
2020-06-13 07:34:01 +0800

    improve scripts/update_res.py and update assets

--- data/update_res.py
+++ data/update_res.py
@@ -4,11 +4,10 @@ AWTK_ROOT='$AWTK_ROOT'
 sys.path.append(os.path.join(AWTK_ROOT, "scripts"))
 import update_res_common as common
 
-APP_ROOT=os.getcwd()
+APP_ROOT = os.getcwd()
 
-ASSETS_ROOT=common.joinPath(APP_ROOT, 'assets')
-ASSET_C=common.joinPath(APP_ROOT, 'assets.c')
+ASSETS_ROOT = common.join_path(APP_ROOT, 'assets')
+ASSET_C = common.join_path(APP_ROOT, 'assets.c')
 
 common.init(AWTK_ROOT, ASSETS_ROOT, 'default', ASSET_C);
-
-common.updateRes()
+common.update_res()
