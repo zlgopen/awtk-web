de15ee5c72c2c7eb1f2f8350b1633b6a975cd373
lixianjing <xianjimli@hotmail.com>
2024-10-20 10:56:38 +0800

    remove image_copy in  dummy.c

--- src/c/dummy.c
+++ src/c/dummy.c
@@ -4,8 +4,4 @@
 #include "base/main_loop.h"
 #include "base/input_method.h"
 
-ret_t image_copy(bitmap_t *dst, bitmap_t *src, rect_t *src_r, xy_t dx,
-                 xy_t dy) {
-  return RET_OK;
-}
 
