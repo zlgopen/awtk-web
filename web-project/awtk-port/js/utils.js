ac6faadc48d4d072396b90cf50fdd1da988c5365
lixianjing <xianjimli@hotmail.com>
2024-11-23 17:16:41 +0800

    refactor

--- web-project/awtk-port/js/utils.js
+++ web-project/awtk-port/js/utils.js
@@ -2,20 +2,7 @@ function pointerToString(pointer) {
   return pointer && Module.UTF8ToString(pointer, 1024) || null;
 }
 
-function wstrToString(wcharPtr) {
-  let str = "";
-  let i = 0;
-  while(true) {
-    const wchar = Module.getValue(wcharPtr + i * 4, "i32");
-
-    if (wchar) {
-      str += String.fromCharCode(wchar);
-      i++;
-    } else {
-      break;
-    }
-  }
-
-  return str;
+function wstrToString(pointer) {
+  return pointer && Module.UTF32ToString(pointer) || null;
 }
 
